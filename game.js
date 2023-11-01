2247c9d546cdb22530c62dd71bc047298242329c
Premapriya1905 <Premapriya1905@gmail.com>
2023-11-01 15:28:39 +0530

    numeron

--- game.js
+++ game.js
@@ -1,5 +1,74 @@
 // Iteration 2: Generate 2 random number and display it on the screen
-
+const gen1=document.getElementById("number1");
+const gen2=document.getElementById("number2");
+var num1=Math.floor(Math.random() * 101);
+var num2=Math.floor(Math.random() * 101);
+gen1.innerHTML =num1 ;
+gen2.innerHTML=num2;
 // Iteration 3: Make the options button functional
+var greaterbtn=document.getElementById("greater-than")
+var lesserbtn=document.getElementById("lesser-than")
+var equalbtn=document.getElementById("equal-to")
+var time_interval;
+const randomNumbers = () => {
+    if(time_interval){
+        clearInterval(time_interval);
+    }
+
+
+    num1=Math.floor(Math.random() * 101);
+    num2=Math.floor(Math.random() * 101);
+    gen1.innerHTML =num1;
+    gen2.innerHTML=num2;
+
+    count = 6;
+    time_interval = setInterval(time, 1000);
+}
+randomNumbers()
+
+var score=0;
+var count = 6; 
+function score_update(){
+    localStorage.setItem("score", score);
+    window.location.href = "./gameover.html";
+}
+
+greaterbtn.addEventListener("click" , ()=> {
+    if(num1>num2){
+        score++
+        randomNumbers()
+    }else {
+        score_update()
+    }
+    
+})
+
+lesserbtn.addEventListener("click",()=> {
+    if(num1<num2){
+        score++
+        randomNumbers()
+    }else{
+        score_update()
+    }
+})
+
+equalbtn.addEventListener("click",()=>{
+    if(num1==num2){
+        score++
+        randomNumbers()
+    }else{
+        score_update()
+    }
+})
 
 // Iteration 4: Build a timer for the game
+var count = 6;
+var timer =document.getElementById("timer")
+function time(){
+    if (count > 0){
+        count--;
+        timer.innerHTML = count;
+    }else{
+        window.location.href = "gameover.html";
+    }
+}
--- score.js
+++ score.js
@@ -1,1 +1,10 @@
 // Iteration 5: Store the player score and display it on the game over screen
+var score_display=document.getElementById("score-board");
+
+let  x = localStorage.getItem("score");
+score_display.textContent = x;
+
+var playagain = document.getElementById("play-again-button")
+playagain.addEventListener("click", () => {
+    window.location.href = "game.html"
+})
